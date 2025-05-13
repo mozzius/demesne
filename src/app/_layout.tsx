8a9d3696c8c6e7a6157e27872164dd8db2ab7fb0
Samuel Newman <mozzius@protonmail.com>
2025-05-13 15:17:42 -0700

    dep up

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout() {
             <Stack.Screen
               name="index"
               options={{
-                title: "Demesne",
+                title: "My Accounts",
                 ...coolLargeTitleEffect,
               }}
             />
